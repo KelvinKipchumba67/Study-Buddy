9d061e49ca86ff7c0f980589ff780ec0b0aa3cbc
MGX <admin@mgx.dev>
2025-09-02 02:26:42 -0700

    Restored Firebase initialization code.

--- react_template/src/utils/firebase.js
+++ react_template/src/utils/firebase.js
@@ -1,3 +1,8 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAuYCJWHjnFlCuTPFhbrmk-fWjAVIeizCI",
   authDomain: "study-buddy-1d8f4.firebaseapp.com",
@@ -7,3 +12,15 @@ const firebaseConfig = {
   appId: "1:635191586350:web:395ce615e075c14400f9bf",
   measurementId: "G-MKDTCD3THK"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
+export default app;
